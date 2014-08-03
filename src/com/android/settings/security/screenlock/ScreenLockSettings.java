1a2273362fba5587a6103183cce7d438a58fa4b4
Adnan <adnan@cyngn.com>
2014-08-03 15:52:05 -0700

    Add option to scramble pin layout when unlocking (1/2)
    @mydongistiny: Ported to Pie
    Change-Id: I3e2c200a0a31d3c765831bc30280029a50c88051
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ib0c34ba63d6c7e5691bd119dac63617292ab5dac
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -76,6 +76,8 @@ public class ScreenLockSettings extends DashboardFragment
         controllers.add(new LockAfterTimeoutPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
+        controllers.add(new ScramblePinPreferenceController(
+                context, MY_USER_ID, lockPatternUtils));
         return controllers;
     }
 
