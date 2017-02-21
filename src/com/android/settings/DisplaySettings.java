72bb5e4404074b72cc177e9667ea5b8ad5078a6f
Chris Lahaye <dev@chrislahaye.com>
2017-02-21 17:33:16 -0500

    Settings: Add pocket lock toggle
    AICP Pie: Modified defaults to false.
    	Brought up to 10 with androidx
    
    @jhenrique09 edits: Adapt strings, and also use config_pocketModeSupported on isAvailable
    @Stallix: Use strings from A11 commit: https://github.com/Evolution-X/packages_apps_Settings/commit/911c7c3157e6b2e8e6c3352038df4785f590d3ab
    
    Change-Id: I398c8f0be23c27cbcf1fb57eed44a801b31bc6c9
    
    Settings: Remove pocket lock toggle icons
    
     * Also move it to lockscreen settings
    
    Change-Id: Iae8a6b4739c317e723c0cfad46db5ceb494f74f8
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -25,10 +25,11 @@ import androidx.preference.Preference;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
+import com.android.settings.display.EnableBlursPreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
+import com.android.settings.display.PocketJudgePreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
-import com.android.settings.display.EnableBlursPreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -88,9 +89,10 @@ public class DisplaySettings extends DashboardFragment {
             Context context, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new CameraGesturePreferenceController(context));
-        controllers.add(new LiftToWakePreferenceController(context));
-        controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new EnableBlursPreferenceController(context));
+        controllers.add(new LiftToWakePreferenceController(context));
+        controllers.add(new PocketJudgePreferenceController(context));
+        controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
