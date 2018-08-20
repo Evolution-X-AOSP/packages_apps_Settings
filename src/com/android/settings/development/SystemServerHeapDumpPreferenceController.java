0a1759f462698bbe70417f92ed94eb9207b897ba
Jason Edson <jaysonedson@gmail.com>
2018-08-20 15:44:29 -0700

    DevelopmentSettings: Remove system update and other prefs
    Moved to just disabling most of the controllers for android10
    instead of deleting all the preferences.
    
    Change-Id: If4d73b6c5351d8f39fb672713dbe26ff9a225240
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/development/SystemServerHeapDumpPreferenceController.java
+++ src/com/android/settings/development/SystemServerHeapDumpPreferenceController.java
@@ -53,8 +53,7 @@ public class SystemServerHeapDumpPreferenceController extends DeveloperOptionsPr
 
     @Override
     public boolean isAvailable() {
-        return Build.IS_DEBUGGABLE
-                && !mUserManager.hasUserRestriction(UserManager.DISALLOW_DEBUGGING_FEATURES);
+        return false;
     }
 
     @Override
