d5149733581fb6ebc70f3350d263404916cef4e3
Vachounet <vachounet@live.fr>
2018-09-12 11:50:31 +0200

    Settings: PowerUsageSummary: open advanced usage on header click
    Change-Id: Id54089a47836eef127bd138da86dc115fc80f0c5
    Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -27,6 +27,7 @@ import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.provider.Settings.Global;
+import androidx.preference.Preference;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.loader.app.LoaderManager;
@@ -36,6 +37,7 @@ import androidx.preference.Preference;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.Utils;
+import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.fuelgauge.BatteryHeaderPreferenceController;
 import com.android.settings.fuelgauge.BatteryInfo;
 import com.android.settings.fuelgauge.BatteryInfoLoader;
@@ -189,6 +191,19 @@ public class PowerUsageSummary extends PowerUsageBase implements
         if (!getResources().getBoolean(R.bool.config_supportSmartCharging)) {
             getPreferenceScreen().removePreference(mSmartCharging);
         }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (BatteryHeaderPreferenceController.KEY_BATTERY_HEADER.equals(preference.getKey())) {
+            new SubSettingLauncher(getContext())
+                        .setDestination(PowerUsageAdvanced.class.getName())
+                        .setSourceMetricsCategory(getMetricsCategory())
+                        .setTitleRes(R.string.advanced_battery_title)
+                        .launch();
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 
     @Override
