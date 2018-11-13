96229f22dc350f097a3ef44e8efa45e55854d742
bigrushdog <randall.rushing@gmail.com>
2018-11-13 05:13:16 +0000

    Add Font preference for font manager
    Fix some stuff
    
    Change-Id: Ifb6eb578e6b6b22ca15baa7cb8f8db32bebe6e15
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -35,6 +35,7 @@ import com.android.settings.display.EnableBlursPreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
+import com.android.settings.display.FontPickerPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
@@ -99,6 +100,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new LiveDisplayPreferenceController(context));
+        controllers.add(new FontPickerPreferenceController(context, lifecycle));
         return controllers;
     }
 
