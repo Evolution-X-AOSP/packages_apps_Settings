55e904bce6046bc71da9119c64e764b7ffa265ec
Ali B <abittin@gmail.com>
2019-05-13 20:10:41 +0300

    Settings: remove LiveDisplay if not supported
    Change-Id: I0032b88e1fcaf918935b861434c8b4a4cf090cd9

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -27,6 +27,7 @@ import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.HomeWakeButtonPreferenceController;
+import com.android.settings.display.LiveDisplayPreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
@@ -103,6 +104,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new LiveDisplayPreferenceController(context));
         return controllers;
     }
 
