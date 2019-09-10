d0095cac85ef33463c6de215ea6583056c34223b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 12:20:19 -0300

    FeatureFactoryImpl: Implement AccountFeatureProviderGoogleImpl
    Required for avatar on dashboard
    
    Change-Id: Icc73d73849ad0cbe46724eaa990417c4346864cf
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,4 +1,16 @@
 package com.google.android.settings.overlay;
 
+import com.android.settings.accounts.AccountFeatureProvider;
+import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
+
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
+    private AccountFeatureProvider mAccountFeatureProvider;
+
+    @Override
+    public AccountFeatureProvider getAccountFeatureProvider() {
+        if (mAccountFeatureProvider == null) {
+            mAccountFeatureProvider = new AccountFeatureProviderGoogleImpl();
+        }
+        return mAccountFeatureProvider;
+    }
 }
