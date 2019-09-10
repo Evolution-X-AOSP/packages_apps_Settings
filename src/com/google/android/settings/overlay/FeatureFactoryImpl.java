2f78709b41201458df2d5e1bce432a0eee3e25be
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 13:22:03 -0300

    FeatureFactoryImpl: Implement ApplicationFeatureProviderGoogleImpl
    Required for displaying app times from wellbeing
    
    Strings taken from Pixel 3
    
    Change-Id: Ie7d1e8993cb8a9d3829ededec40a8e9782a4337b
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,16 +1,21 @@
 package com.google.android.settings.overlay;
 
+import android.app.AppGlobals;
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 
 import com.android.settings.accounts.AccountFeatureProvider;
 import com.android.settings.search.SearchFeatureProvider;
 import com.android.settings.dashboard.suggestions.SuggestionFeatureProvider;
+import com.android.settings.applications.ApplicationFeatureProvider;
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
+import com.google.android.settings.applications.ApplicationFeatureProviderGoogleImpl;
 import com.google.android.settings.dashboard.suggestions.SuggestionFeatureProviderGoogleImpl;
 import com.google.android.settings.search.SearchFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
     private AccountFeatureProvider mAccountFeatureProvider;
+    private ApplicationFeatureProvider mApplicationFeatureProvider;
     private SearchFeatureProvider mSearchFeatureProvider;
     private SuggestionFeatureProvider mSuggestionFeatureProvider;
 
@@ -36,5 +41,17 @@ public final class FeatureFactoryImpl extends com.android.settings.overlay.Featu
             mSuggestionFeatureProvider = new SuggestionFeatureProviderGoogleImpl(context.getApplicationContext());
         }
         return mSuggestionFeatureProvider;
+    }
+
+    @Override
+    public ApplicationFeatureProvider getApplicationFeatureProvider(Context context) {
+        if (mApplicationFeatureProvider == null) {
+            Context applicationContext = context.getApplicationContext();
+            DevicePolicyManager dpManager =
+                (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+            mApplicationFeatureProvider = new ApplicationFeatureProviderGoogleImpl(applicationContext,
+                    applicationContext.getPackageManager(), AppGlobals.getPackageManager(), dpManager);
+        }
+        return mApplicationFeatureProvider;
     }
 }
