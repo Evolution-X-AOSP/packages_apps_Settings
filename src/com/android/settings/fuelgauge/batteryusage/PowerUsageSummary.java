d7e20e091f6ef6f31fad106d9ad30518c2e93d7c
DennySPB <dennyspb@gmail.com>
2019-09-15 04:17:29 +0200

    Settings: Show battery temp data on Battery settings
    Change-Id: Iff2ed19f59f9b9edcc466840a1ba0d385e29ff07
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    -----
    
    Settings: Battery: Make battery temp preference looks like others
    
    Change-Id: I5c0dbbfe9d0beddec2274fbd593080384998af4a
    
    -----
    
    Settings: Battery: Fixed decimal value of battery temperature
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic3bf76f192514cac61deaf236335e6fdb274a7f4
    
    -----
    
    Settings: Battery: Show unavailable instead of 0.0°C for battery temp
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -64,6 +64,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @VisibleForTesting
     static final String KEY_BATTERY_USAGE = "battery_usage_summary";
 
+    private static final String KEY_BATTERY_TEMP = "battery_temp";
+
     @VisibleForTesting
     PowerUsageFeatureProvider mPowerFeatureProvider;
     @VisibleForTesting
@@ -72,6 +74,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
     LayoutPreference mBatteryLayoutPref;
     @VisibleForTesting
     BatteryInfo mBatteryInfo;
+    @VisibleForTesting
+    PowerGaugePreference mBatteryTempPref;
 
     @VisibleForTesting
     BatteryHeaderPreferenceController mBatteryHeaderPreferenceController;
@@ -161,6 +165,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         initFeatureProvider();
         initPreference();
 
+        mBatteryTempPref = (PowerGaugePreference) findPreference(KEY_BATTERY_TEMP);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
 
         if (Utils.isBatteryPresent(getContext())) {
@@ -229,6 +234,11 @@ public class PowerUsageSummary extends PowerUsageBase implements
             restartBatteryTipLoader();
         } else {
             mNeedUpdateBatteryTip = true;
+        }
+        if (BatteryInfo.batteryTemp != 0f) {
+            mBatteryTempPref.setSummary(BatteryInfo.batteryTemp / 10 + " °C");
+        } else {
+            mBatteryTempPref.setSummary(getResources().getString(R.string.status_unavailable));
         }
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
