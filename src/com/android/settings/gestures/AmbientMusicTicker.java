fe320239bed04f3cb45ae750a6b8d922bad43d97
Stallix <team.evolutionx.rom@gmail.com>
2019-12-31 07:27:23 +0000

    Settings: Ambient Music Ticker: Several improvements from Pie
    * Add into a separate fragment: https://github.com/Evolution-X-Legacy/packages_apps_Evolution/commit/44cb552c5c7f5e31fd3016a90c18cb14d74ea090
    * Add an illustration: https://github.com/Evolution-X-Legacy/packages_apps_Evolution/commit/77cfc8b3d3834df1507aedb68b9fe2a0fcc391c8
    * Improve footer text wording: https://github.com/Evolution-X-Legacy/packages_apps_Evolution/commit/e429a7b856fc95fb6884d74cfac62ce89143a6c8
    * Make use of SystemSettingSwitchPreference
    
    Co-authored-by: ezio84 <brabus84@gmail.com>
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

---
+++ src/com/android/settings/gestures/AmbientMusicTicker.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.evolution.settings.preference.SystemSettingMasterSwitchPreference;
+
+public class AmbientMusicTicker extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.ambient_music_ticker);
+
+        getActivity().getActionBar().setTitle(R.string.pulse_on_new_tracks_title);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.ambient_ticker_footer);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVO_SETTINGS;
+    }
+}
