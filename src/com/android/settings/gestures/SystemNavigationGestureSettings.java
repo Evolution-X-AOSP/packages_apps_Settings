430b97f7dbf1c4b9f72f614491b6caa03a55616d
maxwen <max.weninger@gmail.com>
2020-01-22 02:02:20 +0100

    [2/2] Settings: dont block IME space for back gesture
    Change-Id: Iec0d223ecaebdae7123c3188f7963a83cf9be27d
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/gestures/SystemNavigationGestureSettings.java
+++ src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -183,7 +183,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
             p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_SETTING);
             p.setExtraWidgetOnClickListener((v) -> GestureNavigationBackSensitivityDialog
                     .show(this, getBackSensitivity(getContext(), mOverlayManager),
-                    getBackHeight(getContext())));
+                    getBackHeight(getContext()), getBackBlockIme(getContext())));
         } else {
             p.setExtraWidgetVisibility(EXTRA_WIDGET_VISIBILITY_GONE);
         }
@@ -289,6 +289,16 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment {
     static int getBackHeight(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
                 Settings.System.BACK_GESTURE_HEIGHT, 0);
+    }
+
+    static void setBackBlockIme(Context context, boolean blockIme) {
+        Settings.System.putInt(context.getContentResolver(),
+                Settings.System.BACK_GESTURE_BLOCK_IME, blockIme ? 1 : 0);
+    }
+
+    static boolean getBackBlockIme(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.BACK_GESTURE_BLOCK_IME, 1) == 1;
     }
 
     static int getPillToggleState(Context context) {
