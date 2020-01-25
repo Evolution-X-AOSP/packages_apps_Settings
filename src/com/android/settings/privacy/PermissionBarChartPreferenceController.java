5d23775a291f41f8376b085c2e240aff1a9c063a
AnkitGourav <Ankitgourav035@gmail.com>
2020-01-26 02:17:50 +0530

    Settings: Enable PermissionBar Chart
    - force enable PermissionBar Chart for all
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/privacy/PermissionBarChartPreferenceController.java
+++ src/com/android/settings/privacy/PermissionBarChartPreferenceController.java
@@ -89,9 +89,10 @@ public class PermissionBarChartPreferenceController extends BasePreferenceContro
 
     @Override
     public int getAvailabilityStatus() {
-        return DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_PRIVACY,
+        /*return DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_PRIVACY,
                 com.android.settings.Utils.PROPERTY_PERMISSIONS_HUB_ENABLED, true) ?
-                    AVAILABLE_UNSEARCHABLE : UNSUPPORTED_ON_DEVICE;
+                    AVAILABLE_UNSEARCHABLE : UNSUPPORTED_ON_DEVICE;*/
+        return AVAILABLE_UNSEARCHABLE;
     }
 
     @Override
