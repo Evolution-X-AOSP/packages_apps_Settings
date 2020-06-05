d2d453d00f793f5b65effc00071efa4516cf3f65
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-05 17:15:19 +0300

    Settings: Add vibration patterns from OOS [3/3]
    a rewrite of: https://github.com/AOSPA/android_packages_apps_Settings/commit/c4560cafae3f97f09c372c5289869b35c90d3b71
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -239,6 +239,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(new PhoneRingtonePreferenceController(context));
         controllers.add(new AlarmRingtonePreferenceController(context));
         controllers.add(new NotificationRingtonePreferenceController(context));
+        controllers.add(new VibrationPatternPreferenceController(context));
 
         // === Work Sound Settings ===
         controllers.add(new WorkSoundPreferenceController(context, fragment, lifecycle));
