337a67374d1b4704a95a07a4e74fc7c33f521aa5
Alex Cruz <du.alexcruz@gmail.com>
2020-06-25 14:02:16 -0400

    Settings: Make lockscreen timeout configurable [2/2]
    Modeled after the screen timeout, this will control the lockscreen
    timeout.
    
    Change-Id: I5e095ee1c2fa59ab715964ca7ac0cd2f4a4b3ab7
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -33,6 +33,7 @@ import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
+import com.android.settings.display.TimeoutLockscreenPreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -55,6 +56,7 @@ public class DisplaySettings extends DashboardFragment {
     private static final String TAG = "DisplaySettings";
     private static final String NOTCH_CATEGORY = "notch_category";
 
+    private static final String KEY_LOCKSCREEN_TIMEOUT = "lockscreen_timeout";
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
     private static final String KEY_HIGH_TOUCH_SENSITIVITY = "high_touch_sensitivity_enable";
 
@@ -103,6 +105,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new NightModePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new TapToWakePreferenceController(context));
+        controllers.add(new TimeoutLockscreenPreferenceController(context, KEY_LOCKSCREEN_TIMEOUT));
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
