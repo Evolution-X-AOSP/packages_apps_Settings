c0ccb1de3eb002c64f80b0cc00d219a25966a84c
AshutoshSundresh <62063875+AshutoshSundresh@users.noreply.github.com>
2020-07-11 23:03:04 +0530

    BatteryHealth: Make it optional
    * Allows a device maintainer to show/hide the BatteryHealth preference in Power Settings.
    * Helps certain OnePlus devices with unreliable sys nodes
    * Disable it by default
    
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -293,6 +293,20 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         if (!getResources().getBoolean(com.android.internal.R.bool.config_SmartChargingAvailable)) {
             getPreferenceScreen().removePreference(mSmartChargingPref);
         }
+
+        // Check availability of Battery Health
+        Preference mDesignedHealthPref = (Preference) findPreference(KEY_DESIGNED_BATTERY_CAPACITY);
+        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
+            getPreferenceScreen().removePreference(mDesignedHealthPref);
+        }
+        Preference mCurrentHealthPref = (Preference) findPreference(KEY_CURRENT_BATTERY_CAPACITY);
+        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
+            getPreferenceScreen().removePreference(mCurrentHealthPref);
+        }
+        Preference mCyclesHealthPref = (Preference) findPreference(KEY_BATTERY_CHARGE_CYCLES);
+        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
+            getPreferenceScreen().removePreference(mCyclesHealthPref);
+        }
     }
 
     @Override
