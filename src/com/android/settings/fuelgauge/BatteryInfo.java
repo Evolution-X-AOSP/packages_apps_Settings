f0f6d51a30c397175651b743c826590a68aa7413
Ali B <abittin@gmail.com>
2020-12-04 15:53:37 +0300

    Settings: Display 'VOOC charging' for vooc charge in battery settings [2/2]
    Change-Id: I0dd144a0202dc7995390e8332e5e7cfc3014b75e

--- src/com/android/settings/fuelgauge/BatteryInfo.java
+++ src/com/android/settings/fuelgauge/BatteryInfo.java
@@ -282,6 +282,8 @@ public class BatteryInfo {
                 BatteryManager.EXTRA_DASH_CHARGER, false);
         final boolean warpChargeStatus = batteryBroadcast.getBooleanExtra(
                 BatteryManager.EXTRA_WARP_CHARGER, false);
+        final boolean voocChargeStatus = batteryBroadcast.getBooleanExtra(
+                BatteryManager.EXTRA_VOOC_CHARGER, false);
         info.discharging = false;
         info.suggestionLabel = null;
         if (info.isOverheated && status != BatteryManager.BATTERY_STATUS_FULL) {
@@ -303,6 +305,9 @@ public class BatteryInfo {
             } else if (warpChargeStatus) {
                 info.remainingLabel = context.getString(
                         R.string.power_remaining_warp_charging_duration_only, timeString);
+            } else if (voocChargeStatus) {
+                info.remainingLabel = context.getString(
+                        R.string.power_remaining_vooc_charging_duration_only, timeString);
             } else {
                 info.remainingLabel = context.getString(
                         R.string.power_remaining_charging_duration_only, timeString);
