a767992530e76040e6a119d518cf9357f4b5cafc
Henrique Silva <jhenrique09.mcz@hotmail.com>
2021-03-20 16:40:23 +0530

    Settings: DisplayCutoutForceFullscreenSettings: Don't force close app
    Show a toast informing the user that app needs restart
    
    Change-Id: Ie78f9dbd85c94e05128be6f0d0e915c3ef2d2b21
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Aman Singh <amankumarsingh1412@gmail.com>
    Signed-off-by: Stallix <joey@evolution-x.org>

--- src/com/android/settings/display/DisplayCutoutForceFullscreenSettings.java
+++ src/com/android/settings/display/DisplayCutoutForceFullscreenSettings.java
@@ -36,6 +36,7 @@ import android.widget.ListView;
 import android.widget.SectionIndexer;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.settingslib.applications.ApplicationsState;
 
@@ -53,7 +54,6 @@ import java.util.Map;
 public class DisplayCutoutForceFullscreenSettings extends SettingsPreferenceFragment
         implements ApplicationsState.Callbacks {
 
-    private ActivityManager mActivityManager;
     private AllPackagesAdapter mAllPackagesAdapter;
     private ApplicationsState mApplicationsState;
     private ApplicationsState.Session mSession;
@@ -71,8 +71,6 @@ public class DisplayCutoutForceFullscreenSettings extends SettingsPreferenceFrag
         mApplicationsState = ApplicationsState.getInstance(getActivity().getApplication());
         mSession = mApplicationsState.newSession(this);
         mSession.onResume();
-        mActivityManager = (ActivityManager) getActivity().getSystemService(
-                Context.ACTIVITY_SERVICE);
         mActivityFilter = new ActivityFilter(getActivity().getPackageManager());
         mAllPackagesAdapter = new AllPackagesAdapter(getActivity());
 
@@ -239,10 +237,9 @@ public class DisplayCutoutForceFullscreenSettings extends SettingsPreferenceFrag
                     } else {
                         mCutoutForceFullscreenSettings.removeApp(appEntry.info.packageName);
                     }
-                    try{
-                        mActivityManager.forceStopPackage(appEntry.info.packageName);
-                    }catch(Exception ignored){
-                    }
+                    Toast.makeText(getActivity(),
+                            getActivity().getString(R.string.display_cutout_force_fullscreen_restart_app),
+                            Toast.LENGTH_SHORT).show();
                 });
             } else {
                 holder = (ViewHolder) convertView.getTag();
