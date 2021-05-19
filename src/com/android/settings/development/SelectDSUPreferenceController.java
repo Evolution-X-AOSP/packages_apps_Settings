a4c4de727d22c6179ed9c9f17d304d0fce61c985
ezio84 <brabus84@gmail.com>
2021-05-19 03:07:36 +0000

    development: disable DSULoader
    * doesn't work so might as well hide it
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/development/SelectDSUPreferenceController.java
+++ src/com/android/settings/development/SelectDSUPreferenceController.java
@@ -40,6 +40,11 @@ class SelectDSUPreferenceController extends DeveloperOptionsPreferenceController
         return DSU_LOADER_KEY;
     }
 
+    @Override
+    public boolean isAvailable() {
+        return false;
+    }
+
     private boolean isDSURunning() {
         return SystemProperties.getBoolean("ro.gsid.image_running", false);
     }
