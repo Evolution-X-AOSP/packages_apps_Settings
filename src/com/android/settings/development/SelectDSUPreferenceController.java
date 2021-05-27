fb4c3a3b95fe622898ba76e15174f546541c0863
Joey Huab <joey@evolution-x.org>
2021-05-27 17:43:10 +0900

    Revert "development: disable DSULoader"
    This reverts commit d641da618b891b40c95c9e17a10bfa780c9f3fa3.

--- src/com/android/settings/development/SelectDSUPreferenceController.java
+++ src/com/android/settings/development/SelectDSUPreferenceController.java
@@ -38,11 +38,6 @@ class SelectDSUPreferenceController extends DeveloperOptionsPreferenceController
         return DSU_LOADER_KEY;
     }
 
-    @Override
-    public boolean isAvailable() {
-        return false;
-    }
-
     private boolean isDSURunning() {
         return SystemProperties.getBoolean("ro.gsid.image_running", false);
     }
