7d566bd03f965b3219e43c2a89df3fb3b41d8fab
jhenrique09 <jhsv09@gmail.com>
2021-06-18 11:56:26 -0300

    [2/2] Allow changing face unlock method when locked
    Change-Id: I143bde86acc2a4ada27c0ef739eb87d20cfc6512
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/biometrics/face/FaceSettings.java
+++ src/com/android/settings/biometrics/face/FaceSettings.java
@@ -73,7 +73,6 @@ public class FaceSettings extends DashboardFragment {
     private FaceSettingsAttentionPreferenceController mAttentionController;
     private FaceSettingsRemoveButtonPreferenceController mRemoveController;
     private FaceSettingsEnrollButtonPreferenceController mEnrollController;
-    private FaceSettingsLockscreenBypassPreferenceController mLockscreenController;
     private FaceSettingsRedoPreferenceController mRedoController;
     private List<AbstractPreferenceController> mControllers;
 
@@ -170,10 +169,8 @@ public class FaceSettings extends DashboardFragment {
         Preference appPref = findPreference(FaceSettingsAppPreferenceController.KEY);
         Preference attentionPref = findPreference(FaceSettingsAttentionPreferenceController.KEY);
         Preference confirmPref = findPreference(FaceSettingsConfirmPreferenceController.KEY);
-        Preference bypassPref =
-                findPreference(mLockscreenController.getPreferenceKey());
         mTogglePreferences = new ArrayList<>(
-                Arrays.asList(keyguardPref, appPref, attentionPref, confirmPref, bypassPref));
+                Arrays.asList(keyguardPref, appPref, attentionPref, confirmPref));
 
         mRemoveButton = findPreference(FaceSettingsRemoveButtonPreferenceController.KEY);
         mEnrollButton = findPreference(FaceSettingsEnrollButtonPreferenceController.KEY);
@@ -195,7 +192,6 @@ public class FaceSettings extends DashboardFragment {
         // Don't show keyguard controller for work profile settings.
         if (mUserManager.isManagedProfile(mUserId)) {
             removePreference(FaceSettingsKeyguardPreferenceController.KEY);
-            removePreference(mLockscreenController.getPreferenceKey());
         }
 
         if (savedInstanceState != null) {
@@ -206,9 +202,6 @@ public class FaceSettings extends DashboardFragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
-        mLockscreenController = use(FaceSettingsLockscreenBypassPreferenceController.class);
-        mLockscreenController.setUserId(mUserId);
     }
 
     @Override
