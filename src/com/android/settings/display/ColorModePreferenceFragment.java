36b1e727fab1fae2460fc2a262688cb9fa69e79f
Danny Lin <danny@kdrag0n.dev>
2021-07-08 03:04:30 -0700

    Add sliders for color balance adjustment via ColorDisplayManager
    These sliders allow the user to adjust RGB color balance using our
    simple ColorDisplayManager transforms.
    
    Requires frameworks/base commit: "display: Add simple RGB color balance transform"
    
    Change-Id: I8205ab6e0c354ab77b58d55c5ca2c51fb473cf30
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/display/ColorModePreferenceFragment.java
+++ src/com/android/settings/display/ColorModePreferenceFragment.java
@@ -200,6 +200,21 @@ public class ColorModePreferenceFragment extends RadioButtonPickerFragment {
     }
 
     @Override
+    public void updateCandidates() {
+        super.updateCandidates();
+
+        PreferenceScreen screen = getPreferenceScreen();
+        getPreferenceManager().inflateFromResource(screen.getContext(), R.xml.color_mode_settings,
+                screen);
+
+        for (int channel = 0; channel < 3; channel++) {
+            ColorBalancePreferenceController controller = new ColorBalancePreferenceController(
+                    screen.getContext(), ColorBalancePreferenceController.channelToKey(channel));
+            controller.displayPreference(screen);
+        }
+    }
+
+    @Override
     protected void addStaticPreferences(PreferenceScreen screen) {
         final LayoutPreference preview = new LayoutPreference(screen.getContext(),
                 R.layout.color_mode_preview);
