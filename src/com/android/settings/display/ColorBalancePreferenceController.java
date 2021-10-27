0f53bc55d472b136587cf570e573ac75d5349d64
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-10-27 17:10:25 +0530

    Settings: Show current value of color balance sliders
     * Refactor it into SeekbarPreference and set summary
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I5b0c05ec6791339f55bff2f2ef9c63b640a1007e

--- src/com/android/settings/display/ColorBalancePreferenceController.java
+++ src/com/android/settings/display/ColorBalancePreferenceController.java
@@ -21,14 +21,16 @@ import android.hardware.display.ColorDisplayManager;
 import android.text.TextUtils;
 
 import androidx.preference.PreferenceScreen;
+import androidx.preference.SeekBarPreference;
 
 import com.android.settings.core.SliderPreferenceController;
-import com.android.settings.widget.SeekBarPreference;
 
 public class ColorBalancePreferenceController extends SliderPreferenceController {
 
     private final ColorDisplayManager mColorDisplayManager;
     private final int mChannel;
+
+    private SeekBarPreference mPreference;
 
     public ColorBalancePreferenceController(Context context, String key) {
         super(context, key);
@@ -55,11 +57,12 @@ public class ColorBalancePreferenceController extends SliderPreferenceController
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
-        final SeekBarPreference preference = screen.findPreference(getPreferenceKey());
-        preference.setContinuousUpdates(true);
-        preference.setMax(getMax());
-        preference.setMin(getMin());
-        preference.setProgress(getSliderPosition());
+        mPreference = screen.findPreference(getPreferenceKey());
+        mPreference.setUpdatesContinuously(true);
+        mPreference.setMax(getMax());
+        mPreference.setMin(getMin());
+        mPreference.setValue(getSliderPosition());
+        mPreference.setSummary(Integer.toString(getSliderPosition()));
     }
 
     @Override
@@ -69,6 +72,7 @@ public class ColorBalancePreferenceController extends SliderPreferenceController
 
     @Override
     public boolean setSliderPosition(int position) {
+        mPreference.setSummary(Integer.toString(position));
         return mColorDisplayManager.setColorBalanceChannel(mChannel, position);
     }
 
