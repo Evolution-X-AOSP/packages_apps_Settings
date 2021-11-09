a7b2f8e066f1bc0095f0d101073cda178d5eb293
jhenrique09 <jhsv09@gmail.com>
2021-11-09 15:50:12 -0300

    Settings: Force disable top level support preference
    Change-Id: I89b50a03bd8e73b636539b8a7ff465dca91c123e

--- src/com/android/settings/support/SupportPreferenceController.java
+++ src/com/android/settings/support/SupportPreferenceController.java
@@ -44,7 +44,7 @@ public class SupportPreferenceController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        return mSupportFeatureProvider == null ? UNSUPPORTED_ON_DEVICE : AVAILABLE;
+        return UNSUPPORTED_ON_DEVICE/*mSupportFeatureProvider == null ? UNSUPPORTED_ON_DEVICE : AVAILABLE*/;
     }
 
     @Override
