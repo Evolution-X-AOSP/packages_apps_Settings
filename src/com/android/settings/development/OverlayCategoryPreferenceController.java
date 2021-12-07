6ab4bb473ee839c6c2b8cf2e3ccea4147cd4169b
sb6596 <shubhamprince111@gmail.com>
2021-12-07 15:25:58 +0000

    Settings: Make Icon Shapes overlay persistent across reboots
     * Similar to 1763ec59985920e2874de1926aacb2f07f1c972a.
    
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Change-Id: I603b4cb92f13d2f77fc40369de33c1a01ba606c1
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/android/settings/development/OverlayCategoryPreferenceController.java
+++ src/com/android/settings/development/OverlayCategoryPreferenceController.java
@@ -57,6 +57,7 @@ public class OverlayCategoryPreferenceController extends DeveloperOptionsPrefere
         implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
     private static final String TAG = "OverlayCategoryPC";
     private static final String FONT_KEY = "android.theme.customization.font";
+    private static final String ADAPTIVE_ICON_SHAPE_KEY = "android.theme.customization.adaptive_icon_shape";
 
     @VisibleForTesting
     static final String PACKAGE_DEVICE_DEFAULT = "package_device_default";
@@ -65,6 +66,7 @@ public class OverlayCategoryPreferenceController extends DeveloperOptionsPrefere
     private final IOverlayManager mOverlayManager;
     private final boolean mAvailable;
     private final boolean mIsFonts;
+    private final boolean mIsAdaptiveIconShape;
     private final String mCategory;
     private final PackageManager mPackageManager;
     private final String mDeviceDefaultLabel;
@@ -81,6 +83,7 @@ public class OverlayCategoryPreferenceController extends DeveloperOptionsPrefere
         mAvailable = overlayManager != null && !getOverlayInfos().isEmpty();
         mDeviceDefaultLabel = mContext.getString(R.string.overlay_option_device_default);
         mIsFonts = FONT_KEY.equals(category);
+        mIsAdaptiveIconShape = ADAPTIVE_ICON_SHAPE_KEY.equals(category);
     }
 
     public OverlayCategoryPreferenceController(Context context, String category) {
@@ -137,7 +140,7 @@ public class OverlayCategoryPreferenceController extends DeveloperOptionsPrefere
         Log.w(TAG, "setOverlay packageNames=" + packageNames.toString());
         Log.w(TAG, "setOverlay label=" + label);
 
-        if (mIsFonts) {
+        if (mIsFonts || mIsAdaptiveIconShape) {
             // For fonts we also need to set this setting
             String value = Settings.Secure.getStringForUser(mContext.getContentResolver(),
                     Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES, UserHandle.USER_CURRENT);
