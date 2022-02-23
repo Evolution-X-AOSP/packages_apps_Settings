2345711515033ff1a71e91f29d9d20f3471a174b
Akito Mizukito <akito@evolution-x.org>
2022-02-23 23:12:09 +0900

    Settings: Revert bug report changes
    Revert "Remove feedback and bug report stuff"
    
    This reverts commit 761cfc832ae215347b1e4aafbbb0b1153db8d239.
    
    Revert "Settings: disable all bugreport settings"
    
    This reverts commit 2f9d0c92f961961990021a1651f61d61ffd4e0a3.
    
    Revert "DevelopmentSettings: Remove system update and other prefs"
    
    This reverts commit 19c85e97fad3b86d02779495872b35ff0275d889.
    
    Change-Id: I2fc714490974c500efd3daa3c9774bff48e0417a
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -469,8 +469,8 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
             BluetoothA2dpConfigStore bluetoothA2dpConfigStore) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new MemoryUsagePreferenceController(context));
-        //controllers.add(new BugReportPreferenceController(context));
-        //controllers.add(new BugReportHandlerPreferenceController(context));
+        controllers.add(new BugReportPreferenceController(context));
+        controllers.add(new BugReportHandlerPreferenceController(context));
         controllers.add(new SystemServerHeapDumpPreferenceController(context));
         controllers.add(new LocalBackupPasswordPreferenceController(context));
         controllers.add(new StayAwakePreferenceController(context, lifecycle));
@@ -481,13 +481,14 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new PictureColorModePreferenceController(context, lifecycle));
         controllers.add(new WebViewAppPreferenceController(context));
         controllers.add(new CoolColorTemperaturePreferenceController(context));
+        //controllers.add(new DisableAutomaticUpdatesPreferenceController(context));
         controllers.add(new SelectDSUPreferenceController(context));
         controllers.add(new AdbPreferenceController(context, fragment));
         controllers.add(new ClearAdbKeysPreferenceController(context, fragment));
         controllers.add(new WirelessDebuggingPreferenceController(context, lifecycle));
         controllers.add(new AdbAuthorizationTimeoutPreferenceController(context));
         controllers.add(new LocalTerminalPreferenceController(context));
-        //controllers.add(new BugReportInPowerPreferenceController(context));
+        controllers.add(new BugReportInPowerPreferenceController(context));
         controllers.add(new AutomaticSystemServerHeapDumpPreferenceController(context));
         controllers.add(new MockLocationAppPreferenceController(context, fragment));
         controllers.add(new DebugViewAttributesPreferenceController(context));
@@ -552,6 +553,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new ShortcutManagerThrottlingPreferenceController(context));
         controllers.add(new EnableGnssRawMeasFullTrackingPreferenceController(context));
         controllers.add(new DefaultLaunchPreferenceController(context, "running_apps"));
+        controllers.add(new DefaultLaunchPreferenceController(context, "demo_mode"));
         controllers.add(new DefaultLaunchPreferenceController(context, "quick_settings_tiles"));
         controllers.add(new DefaultLaunchPreferenceController(context, "feature_flags_dashboard"));
         controllers.add(new DefaultUsbConfigurationPreferenceController(context));
