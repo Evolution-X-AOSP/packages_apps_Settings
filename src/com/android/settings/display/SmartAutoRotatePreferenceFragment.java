75c4788c6a37fa6990860df4d0b267bfecd266c4
jhenrique09 <jhsv09@gmail.com>
2022-04-05 01:22:34 +0000

    Settings: Relocate rotation angle settings to another fragment
    Change-Id: I1b72a4bd66164378f4e408f35f163df2b709b694

--- src/com/android/settings/display/SmartAutoRotatePreferenceFragment.java
+++ src/com/android/settings/display/SmartAutoRotatePreferenceFragment.java
@@ -20,6 +20,7 @@ import static com.android.settings.display.SmartAutoRotateController.isRotationR
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +28,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
@@ -55,6 +58,22 @@ public class SmartAutoRotatePreferenceFragment extends DashboardFragment {
     @VisibleForTesting
     static final String AUTO_ROTATE_SWITCH_PREFERENCE_KEY = "auto_rotate_switch";
     private static final String KEY_FOOTER_PREFERENCE = "auto_rotate_footer_preference";
+
+
+    private static final String ROTATION_0_PREF = "display_rotation_0";
+    private static final String ROTATION_90_PREF = "display_rotation_90";
+    private static final String ROTATION_180_PREF = "display_rotation_180";
+    private static final String ROTATION_270_PREF = "display_rotation_270";
+
+    private SwitchPreference mRotation0Pref;
+    private SwitchPreference mRotation90Pref;
+    private SwitchPreference mRotation180Pref;
+    private SwitchPreference mRotation270Pref;
+
+    public static final int ROTATION_0_MODE = 1;
+    public static final int ROTATION_90_MODE = 2;
+    public static final int ROTATION_180_MODE = 4;
+    public static final int ROTATION_270_MODE = 8;
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -86,6 +105,20 @@ public class SmartAutoRotatePreferenceFragment extends DashboardFragment {
             footerPreference.setVisible(isRotationResolverServiceAvailable(activity));
             setupFooter();
         }
+
+        mRotation0Pref = findPreference(ROTATION_0_PREF);
+        mRotation90Pref = findPreference(ROTATION_90_PREF);
+        mRotation180Pref = findPreference(ROTATION_180_PREF);
+        mRotation270Pref = findPreference(ROTATION_270_PREF);
+
+        int mode = Settings.System.getIntForUser(getContentResolver(),
+                        Settings.System.ACCELEROMETER_ROTATION_ANGLES,
+                        ROTATION_0_MODE|ROTATION_90_MODE|ROTATION_270_MODE, UserHandle.USER_CURRENT);
+
+        mRotation0Pref.setChecked((mode & ROTATION_0_MODE) != 0);
+        mRotation90Pref.setChecked((mode & ROTATION_90_MODE) != 0);
+        mRotation180Pref.setChecked((mode & ROTATION_180_MODE) != 0);
+        mRotation270Pref.setChecked((mode & ROTATION_270_MODE) != 0);
         return view;
     }
 
@@ -136,6 +169,32 @@ public class SmartAutoRotatePreferenceFragment extends DashboardFragment {
             });
             pref.setLearnMoreText(getString(R.string.auto_rotate_link_a11y));
         }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mRotation0Pref ||
+                preference == mRotation90Pref ||
+                preference == mRotation180Pref ||
+                preference == mRotation270Pref) {
+            int mode = 0;
+            if (mRotation0Pref.isChecked())
+                mode |= ROTATION_0_MODE;
+            if (mRotation90Pref.isChecked())
+                mode |= ROTATION_90_MODE;
+            if (mRotation180Pref.isChecked())
+                mode |= ROTATION_180_MODE;
+            if (mRotation270Pref.isChecked())
+                mode |= ROTATION_270_MODE;
+            if (mode == 0) {
+                mode |= ROTATION_0_MODE;
+                mRotation0Pref.setChecked(true);
+            }
+            Settings.System.putIntForUser(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.ACCELEROMETER_ROTATION_ANGLES, mode, UserHandle.USER_CURRENT);
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 
     public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
