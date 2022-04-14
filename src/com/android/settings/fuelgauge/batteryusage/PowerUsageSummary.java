57300ce761e5cc0b65af41c2475174faa1e3a85d
Pranav Vashi <neobuddy89@gmail.com>
2022-04-15 00:09:24 +0530

    Add toggle to switch between AOSP/Google battery graphs
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -24,6 +24,8 @@ import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.provider.Settings.Global;
 
 import androidx.annotation.VisibleForTesting;
@@ -94,6 +96,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             restartBatteryInfoLoader();
+            initPreference();
         }
     };
 
@@ -191,6 +194,10 @@ public class PowerUsageSummary extends PowerUsageBase implements
                 Global.getUriFor(Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME),
                 false,
                 mSettingsObserver);
+        getContentResolver().registerContentObserver(
+                Settings.System.getUriFor("battery_24_hrs_stats"),
+                false,
+                mSettingsObserver);
     }
 
     @Override
@@ -265,7 +272,12 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @VisibleForTesting
     void initPreference() {
         mBatteryUsagePreference = findPreference(KEY_BATTERY_USAGE);
-        mBatteryUsagePreference.setSummary(getString(R.string.advanced_battery_preference_summary));
+        boolean isChartGraphEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
+                "battery_24_hrs_stats", 0, UserHandle.USER_CURRENT) != 0;
+        mBatteryUsagePreference.setSummary(
+                isChartGraphEnabled ?
+                        getString(R.string.advanced_battery_preference_summary_with_hours) :
+                        getString(R.string.advanced_battery_preference_summary));
 
         mHelpPreference = findPreference(KEY_BATTERY_ERROR);
         mHelpPreference.setVisible(false);
