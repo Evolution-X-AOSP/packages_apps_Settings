f8e39a87ed6ea477f3c54355c58981c57e028f20
timjosten <timjosten@gmail.com>
2022-06-07 12:51:28 +0900

    Settings: Remove IME space under keyboard feature [2/2]
    Change-Id: I7e322d85d8dbd937cf657d37d0a86bdc7f371c3f
    
    Settings: Improve IME space feature [2/2]
    
    travarilo: Improve strings
    
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Change-Id: Ie7217f392fa4526f84e6124c6ce860dc767df4c8

--- src/com/android/settings/gestures/SystemNavigationGestureSettings.java
+++ src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -131,8 +131,6 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         }
         setIllustrationVideo(mVideoPreference, getDefaultKey());
         setIllustrationClickListener(mVideoPreference, getDefaultKey());
-
-        migrateOverlaySensitivityToSettings(context, mOverlayManager);
     }
 
     @Override
@@ -264,27 +262,6 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         }
     }
 
-    static void migrateOverlaySensitivityToSettings(Context context,
-            IOverlayManager overlayManager) {
-        if (!SystemNavigationPreferenceController.isGestureNavigationEnabled(context)) {
-            return;
-        }
-
-        OverlayInfo info = null;
-        try {
-            info = overlayManager.getOverlayInfo(NAV_BAR_MODE_GESTURAL_OVERLAY, USER_CURRENT);
-        } catch (RemoteException e) { /* Do nothing */ }
-        if (info != null && !info.isEnabled()) {
-            // Enable the default gesture nav overlay. Back sensitivity for left and right are
-            // stored as separate settings values, and other gesture nav overlays are deprecated.
-            setCurrentSystemNavigationMode(overlayManager, KEY_SYSTEM_NAV_GESTURAL);
-            Settings.Secure.putFloat(context.getContentResolver(),
-                    Settings.Secure.BACK_GESTURE_INSET_SCALE_LEFT, 1.0f);
-            Settings.Secure.putFloat(context.getContentResolver(),
-                    Settings.Secure.BACK_GESTURE_INSET_SCALE_RIGHT, 1.0f);
-        }
-    }
-
     @VisibleForTesting
     static String getCurrentSystemNavigationMode(Context context) {
         if (SystemNavigationPreferenceController.isGestureNavigationEnabled(context)) {
@@ -296,12 +273,11 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         }
     }
 
-    @VisibleForTesting
-    static void setCurrentSystemNavigationMode(IOverlayManager overlayManager, String key) {
-        String overlayPackage = NAV_BAR_MODE_GESTURAL_OVERLAY;
+    void setCurrentSystemNavigationMode(IOverlayManager overlayManager, String key) {
+        String overlayPackage = getNavBarGesturalOverlay();
         switch (key) {
             case KEY_SYSTEM_NAV_GESTURAL:
-                overlayPackage = NAV_BAR_MODE_GESTURAL_OVERLAY;
+                overlayPackage = getNavBarGesturalOverlay();
                 break;
             case KEY_SYSTEM_NAV_2BUTTONS:
                 overlayPackage = NAV_BAR_MODE_2BUTTON_OVERLAY;
@@ -316,6 +292,20 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
+    }
+
+    private String getNavBarGesturalOverlay() {
+        int state = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_IME_SPACE, 0);
+        String overlay = NAV_BAR_MODE_GESTURAL_OVERLAY;
+        switch (state) {
+            case 1:  // narrow
+                overlay += "_narrow_back";
+                break;
+            case 2:  // hidden
+                overlay += "_wide_back";
+        }
+        return overlay;
     }
 
     private void setIllustrationVideo(IllustrationPreference videoPref,
