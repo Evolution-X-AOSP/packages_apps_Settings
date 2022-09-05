e7798abe1095621948672fd518f93305e77e14ca
Jason Edson <jaysonedson@gmail.com>
2022-09-05 04:33:07 -0700

    Settings: Add reverse charging and Pixel battery addons
    This adds reverse charging, adaptive_charging, battery chart,
    enhanced estimates and more from stock Google Roms.
    
    To enable reverse charging the device needs to set
    config_show_reverse_charging to true in an overlay.
    
    Change-Id: Ia70ab6a8ace5037ace3d1aeb73fd1e3af4bcbc8b
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- src/com/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/android/settings/overlay/FeatureFactoryImpl.java
@@ -74,6 +74,9 @@ import com.android.settings.users.UserFeatureProviderImpl;
 import com.android.settings.wifi.WifiTrackerLibProvider;
 import com.android.settings.wifi.WifiTrackerLibProviderImpl;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
+import com.google.android.settings.fuelgauge.BatterySettingsFeatureProviderGoogleImpl;
+import com.google.android.settings.fuelgauge.BatteryStatusFeatureProviderGoogleImpl;
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
 
 /**
  * {@link FeatureFactory} implementation for AOSP Settings.
@@ -123,7 +126,7 @@ public class FeatureFactoryImpl extends FeatureFactory {
     @Override
     public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
         if (mPowerUsageFeatureProvider == null) {
-            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderImpl(
+            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
                     context.getApplicationContext());
         }
         return mPowerUsageFeatureProvider;
@@ -132,7 +135,7 @@ public class FeatureFactoryImpl extends FeatureFactory {
     @Override
     public BatteryStatusFeatureProvider getBatteryStatusFeatureProvider(Context context) {
         if (mBatteryStatusFeatureProvider == null) {
-            mBatteryStatusFeatureProvider = new BatteryStatusFeatureProviderImpl(
+            mBatteryStatusFeatureProvider = new BatteryStatusFeatureProviderGoogleImpl(
                     context.getApplicationContext());
         }
         return mBatteryStatusFeatureProvider;
@@ -141,7 +144,8 @@ public class FeatureFactoryImpl extends FeatureFactory {
     @Override
     public BatterySettingsFeatureProvider getBatterySettingsFeatureProvider(Context context) {
         if (mBatterySettingsFeatureProvider == null) {
-            mBatterySettingsFeatureProvider = new BatterySettingsFeatureProviderImpl(context);
+            mBatterySettingsFeatureProvider = new BatterySettingsFeatureProviderGoogleImpl(
+                    context.getApplicationContext());
         }
         return mBatterySettingsFeatureProvider;
     }
