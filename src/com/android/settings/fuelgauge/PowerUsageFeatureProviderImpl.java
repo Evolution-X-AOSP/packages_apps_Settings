90e13470b1acf08e5e987a75393d703caf563211
Joey Huab <joey@evolution-x.org>
2022-09-13 20:01:30 +0000

    Revert "Port "Battery Usage Alerts" feature from factory images"
    This reverts commit b5a192252bcb2044e9a016661cf4e376c1e8d528.

--- src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImpl.java
+++ src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImpl.java
@@ -33,8 +33,6 @@ import java.util.Set;
 
 public class PowerUsageFeatureProviderImpl implements PowerUsageFeatureProvider {
 
-    private static final String ADDITIONAL_BATTERY_INFO_ACTION = "com.google.android.apps.turbo.SHOW_ADDITIONAL_BATTERY_INFO";
-    private static final String ADDITIONAL_BATTERY_INFO_PACKAGE = "com.google.android.apps.turbo";
     private static final String PACKAGE_CALENDAR_PROVIDER = "com.android.providers.calendar";
     private static final String PACKAGE_MEDIA_PROVIDER = "com.android.providers.media";
     private static final String PACKAGE_SYSTEMUI = "com.android.systemui";
@@ -76,14 +74,12 @@ public class PowerUsageFeatureProviderImpl implements PowerUsageFeatureProvider 
 
     @Override
     public boolean isAdditionalBatteryInfoEnabled() {
-        Intent intent = getAdditionalBatteryInfoIntent();
-        return !mContext.getPackageManager().queryIntentActivities(intent, 0).isEmpty();
+        return false;
     }
 
     @Override
     public Intent getAdditionalBatteryInfoIntent() {
-        Intent intent = new Intent(ADDITIONAL_BATTERY_INFO_ACTION);
-        return intent.setPackage(ADDITIONAL_BATTERY_INFO_PACKAGE);
+        return null;
     }
 
     @Override
