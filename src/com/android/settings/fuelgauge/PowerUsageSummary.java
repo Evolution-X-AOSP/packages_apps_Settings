1727d19ff7417ce56ec84b03709bf469c32c6d7a
Lucchetto <lucchetto.tie@live.com>
2022-09-21 08:57:24 +0800

    Squashed changes for BatteryHealth section
    Settings: add BatteryHealth section
    
    **SDB - Adapt for PowerSummary directly
    
    Change-Id: I55ddc2f2dff019fa91aa1df9c889a87b2ad24df0
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    BatteryHealth: make resources overlayable
    
    Not every device have the charge full & charge full design files at the same location or same name.
    Better to keep it overlayable for maintainer to pick.
    Overlay would be for Settings - overlay/packages/apps/Settings/res/values/x_config.xml
    
    For Example -:
    
        <!-- Battery Health -->
        <string name="config_batDesCap">/sys/class/power_supply/bms/charge_full</string>
        <string name="config_batCurCap">/sys/class/power_supply/bms/charge_now_raw</string>
    
    Change-Id: I1ea1fae71064fe072d03013542927f2df2e0c319
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    BatteryHealth: show Charge Cycles Completed
    
    Overlay -:
        <!-- Battery Health -->
        <string name="config_batChargeCycle">/sys/class/power_supply/bms/cycle_count</string>
    
    Change-Id: If07ee074ad5021b7fef0ecfff086e4d5d7249e70
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    
    BatteryHealth: Make it optional
    
    * Allows a device maintainer to show/hide the BatteryHealth preference in Power Settings.
    * Helps certain OnePlus devices with unreliable sys nodes
    * Enabled by default
    
    Change-Id: Iea321d199187a09706a68e22178ff6626ffef105
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Co-authored-by: AshutoshSundresh <62063875+AshutoshSundresh@users>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -18,6 +18,7 @@ package com.android.settings.fuelgauge;
 
 import static com.android.settings.fuelgauge.BatteryBroadcastReceiver.BatteryUpdateType;
 
+import android.annotation.Nullable;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.database.ContentObserver;
@@ -33,7 +34,7 @@ import android.provider.Settings.Global;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import androidx.preference.Preference;
+import android.util.Log;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.loader.app.LoaderManager;
@@ -53,6 +54,11 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.LayoutPreference;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.Integer;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -71,6 +77,13 @@ public class PowerUsageSummary extends PowerUsageBase implements
     static final String KEY_BATTERY_USAGE = "battery_usage_summary";
 
     private static final String KEY_BATTERY_TEMP = "battery_temp";
+    private static final String KEY_CURRENT_BATTERY_CAPACITY = "current_battery_capacity";
+    private static final String KEY_DESIGNED_BATTERY_CAPACITY = "designed_battery_capacity";
+    private static final String KEY_BATTERY_CHARGE_CYCLES = "battery_charge_cycles";
+
+    private String mBatDesCap;
+    private String mBatCurCap;
+    private String mBatChgCyc;
 
     @VisibleForTesting
     static final int BATTERY_INFO_LOADER = 1;
@@ -79,6 +92,12 @@ public class PowerUsageSummary extends PowerUsageBase implements
 
     static final int MENU_STATS_RESET = Menu.FIRST + 1;
 
+    @VisibleForTesting
+    PowerGaugePreference mCurrentBatteryCapacity;
+    @VisibleForTesting
+    PowerGaugePreference mDesignedBatteryCapacity;
+    @VisibleForTesting
+    PowerGaugePreference mBatteryChargeCycles;
     @VisibleForTesting
     PowerUsageFeatureProvider mPowerFeatureProvider;
     @VisibleForTesting
@@ -179,6 +198,12 @@ public class PowerUsageSummary extends PowerUsageBase implements
         initFeatureProvider();
         initPreference();
 
+        mCurrentBatteryCapacity = (PowerGaugePreference) findPreference(
+                KEY_CURRENT_BATTERY_CAPACITY);
+        mDesignedBatteryCapacity = (PowerGaugePreference) findPreference(
+                KEY_DESIGNED_BATTERY_CAPACITY);
+        mBatteryChargeCycles = (PowerGaugePreference) findPreference(
+                KEY_BATTERY_CHARGE_CYCLES);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
 
         if (Utils.isBatteryPresent(getContext())) {
@@ -194,6 +219,22 @@ public class PowerUsageSummary extends PowerUsageBase implements
         Preference mSmartCharging = (Preference) findPreference("smart_charging_key");
         if (!getResources().getBoolean(R.bool.config_supportSmartCharging)) {
             getPreferenceScreen().removePreference(mSmartCharging);
+        }
+
+        // Check availability of Battery Health
+        Preference mDesignedHealthPref = (Preference) findPreference(KEY_DESIGNED_BATTERY_CAPACITY);
+        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
+            getPreferenceScreen().removePreference(mDesignedHealthPref);
+        }
+
+        Preference mCurrentHealthPref = (Preference) findPreference(KEY_CURRENT_BATTERY_CAPACITY);
+        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
+            getPreferenceScreen().removePreference(mCurrentHealthPref);
+        }
+
+        Preference mCyclesHealthPref = (Preference) findPreference(KEY_BATTERY_CHARGE_CYCLES);
+        if (!getResources().getBoolean(R.bool.config_supportBatteryHealth)) {
+            getPreferenceScreen().removePreference(mCyclesHealthPref);
         }
     }
 
@@ -309,8 +350,15 @@ public class PowerUsageSummary extends PowerUsageBase implements
         } else {
             mNeedUpdateBatteryTip = true;
         }
+
+        mBatDesCap = getResources().getString(R.string.config_batDesCap);
+        mBatCurCap = getResources().getString(R.string.config_batCurCap);
+        mBatChgCyc = getResources().getString(R.string.config_batChargeCycle);
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
+        mCurrentBatteryCapacity.setSubtitle(parseBatterymAhText(mBatCurCap));
+        mDesignedBatteryCapacity.setSubtitle(parseBatterymAhText(mBatDesCap));
+        mBatteryChargeCycles.setSubtitle(parseBatteryCycle(mBatChgCyc));
 
         if (BatteryInfo.batteryTemp != 0f) {
             mBatteryTempPref.setSummary(BatteryInfo.batteryTemp + " \u2103");
@@ -387,6 +435,49 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @Override
     public void onBatteryTipHandled(BatteryTip batteryTip) {
         restartBatteryTipLoader();
+    }
+
+    private String parseBatterymAhText(String file) {
+        try {
+            return Integer.parseInt(readLine(file)) / 1000 + " mAh";
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot read battery capacity from "
+                    + file, ioe);
+        } catch (NumberFormatException nfe) {
+            Log.e(TAG, "Read a badly formatted battery capacity from "
+                    + file, nfe);
+        }
+        return getResources().getString(R.string.status_unavailable);
+    }
+
+    private String parseBatteryCycle(String file) {
+        try {
+            return Integer.parseInt(readLine(file)) + " Cycles";
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot read battery cycle from "
+                    + file, ioe);
+        } catch (NumberFormatException nfe) {
+            Log.e(TAG, "Read a badly formatted battery cycle from "
+                    + file, nfe);
+        }
+        return getResources().getString(R.string.status_unavailable);
+    }
+
+    /**
+    * Reads a line from the specified file.
+    *
+    * @param filename The file to read from.
+    * @return The first line up to 256 characters, or <code>null</code> if file is empty.
+    * @throws IOException If the file couldn't be read.
+    */
+    @Nullable
+    private String readLine(String filename) throws IOException {
+        final BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
+        }
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
