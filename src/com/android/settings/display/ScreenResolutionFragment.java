b5564537e535cd76e279e2e003414157b732f80a
AnierinB <anierin@evolution-x.org>
2022-09-25 07:10:53 +0000

    Settings: ScreenResolutionFragment: Restart SystemUI on res change
    * Avoids issues with various SystemUI elements not being properly scaled after switching resolution.
    * Normally we'd use showSystemUIrestartDialog, although doing so during res change is a bit tricky;
      Simplify it by just restarting SystemUI on every res selection without the Dialog.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/com/android/settings/display/ScreenResolutionFragment.java
+++ src/com/android/settings/display/ScreenResolutionFragment.java
@@ -45,6 +45,8 @@ import com.android.settingslib.widget.FooterPreference;
 import com.android.settingslib.widget.IllustrationPreference;
 import com.android.settingslib.widget.SelectorWithWidgetPreference;
 
+import com.android.internal.util.evolution.EvolutionUtils;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -175,6 +177,7 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
 
         /** Apply the resolution change. */
         mDefaultDisplay.setUserPreferredDisplayMode(getPreferMode(width));
+        EvolutionUtils.restartSystemUi(getContext());
     }
 
     /** Get the key corresponding to the resolution. */
