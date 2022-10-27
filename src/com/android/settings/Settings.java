84f056a6b460de39bacc8a1d994ca560d5ce683b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-28 04:56:09 +0530

    Settings: Expose saved devices fragment via intent
    Used by Bluetooth dialog in SystemUI.
    
    Change-Id: Id38cd39732ad0ab6a20233e200498fbde8b528ed

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -449,4 +449,9 @@ public class Settings extends SettingsActivity {
 
     // LiveDisplay
     public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
+
+    /**
+     * Activity for PreviouslyConnectedDeviceDashboardFragment
+     */
+    public static class PreviouslyConnectedDevicesActivity extends SettingsActivity { /* empty */ }
 }
