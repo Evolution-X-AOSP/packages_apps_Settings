dd93ec4277a5644bdb86d367ada75a61e7118f28
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-12 04:47:39 +0530

    Settings: Do not map Evolver to developer settings
    We don't want preferences injected into developer
    settings fragment in here. One such example is
    "System Tracing".
    
    Fixes: 1bfb75cecc8f6109c3c2a37f0e0672bd9f56eabc ("Settings: Integrate The Evolver into Settings")

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -48,8 +48,6 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
-import com.evolution.settings.EvolutionSettings;
-
 import java.util.Map;
 
 /**
@@ -127,8 +125,6 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_BATTERY_SAVER_SETTINGS);
         PARENT_TO_CATEGORY_KEY_MAP.put(SmartBatterySettings.class.getName(),
                 CategoryKey.CATEGORY_SMART_BATTERY_SETTINGS);
-        PARENT_TO_CATEGORY_KEY_MAP.put(EvolutionSettings.class.getName(),
-                CategoryKey.CATEGORY_EVOLVER);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
