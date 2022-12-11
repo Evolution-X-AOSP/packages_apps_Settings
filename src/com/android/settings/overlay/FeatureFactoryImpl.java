aa7c966d42f054561e3dee5cbe0a4e4d54bcc2a2
Jason Edson <jaysonedson@gmail.com>
2022-12-10 20:58:01 -0800

    Settings: Sync Security Hub pref with pixel-framework
    * Every resources are located under vendor/pixel-framework
      So we don't need to add those here.
    
    * Instead, we will change AOSP Settings' code to correctly reflect pixel-framework's changes.
    
    Original commit: "Settings: Add Security Hub from Pixel devices"
    https://github.com/BenzoRom/packages_apps_Settings/commit/8a3ca46f7ee4bc0535c3fcafcdfd450a0b22a471
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- src/com/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/android/settings/overlay/FeatureFactoryImpl.java
@@ -77,6 +77,8 @@ import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 import com.google.android.settings.fuelgauge.BatterySettingsFeatureProviderGoogleImpl;
 import com.google.android.settings.fuelgauge.BatteryStatusFeatureProviderGoogleImpl;
 import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
+import com.google.android.settings.security.SecurityHubDashboard;
+import com.google.android.settings.security.SecuritySettingsFeatureProviderGoogleImpl;
 
 /**
  * {@link FeatureFactory} implementation for AOSP Settings.
@@ -318,7 +320,8 @@ public class FeatureFactoryImpl extends FeatureFactory {
     @Override
     public SecuritySettingsFeatureProvider getSecuritySettingsFeatureProvider() {
         if (mSecuritySettingsFeatureProvider == null) {
-            mSecuritySettingsFeatureProvider = new SecuritySettingsFeatureProviderImpl();
+            mSecuritySettingsFeatureProvider =
+                    new SecuritySettingsFeatureProviderGoogleImpl(getAppContext());
         }
         return mSecuritySettingsFeatureProvider;
     }
