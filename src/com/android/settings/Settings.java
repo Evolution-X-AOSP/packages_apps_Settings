3a0f9a9d22bfe6bbcf2efa979cfb1741738d4896
Anushek Prasal <anushekprasal@gmail.com>
2022-12-14 19:48:47 +0000

    Settings: Introduce automated Sleep Mode [2/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -495,4 +495,7 @@ public class Settings extends SettingsActivity {
 
     // LiveDisplay
     public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
+
+    // SleepMode
+    public static class SleepModeActivity extends SettingsActivity {}
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -109,6 +109,7 @@ import com.android.settings.fuelgauge.AdvancedPowerUsageDetail;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverScheduleSettings;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverSettings;
 import com.android.settings.fuelgauge.batteryusage.PowerUsageSummary;
+import com.android.settings.fuelgauge.sleepmode.SleepMode;
 import com.android.settings.gestures.ButtonNavigationSettingsFragment;
 import com.android.settings.gestures.DoubleTapPowerSettings;
 import com.android.settings.gestures.DoubleTapScreenSettings;
@@ -384,6 +385,7 @@ public class SettingsGateway {
             WifiTetherClientManager.class.getName(),
             LiveDisplaySettings.class.getName(),
             SmartPixels.class.getName(),
+            SleepMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -433,5 +435,6 @@ public class SettingsGateway {
             Settings.EvolutionSettingsActivity.class.getName(),
             Settings.HeadsUpSettingsActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
+            Settings.SleepModeActivity.class.getName(),
     };
 }
