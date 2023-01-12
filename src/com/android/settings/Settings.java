e8c76241d8f03e247b98b9cc65de61340e22f931
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-12 20:10:28 +0200

    Settings: Declare an activity for WirelessDebuggingFragment
    we want the debugging is connected notification to open this page instead of just dev settings
    
    Change-Id: I30e4eb3c4519e421afd6bcf1d10f35b451baf1c1

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -536,6 +536,7 @@ public class Settings extends SettingsActivity {
     public static class EvolutionSettingsActivity extends SettingsActivity {}
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
     public static class SmartPixelsActivity extends SettingsActivity {}
+    public static class WirelessDebuggingActivity extends SettingsActivity { /* empty */ }
 
     /**
      * Activity for PreviouslyConnectedDeviceDashboardFragment
