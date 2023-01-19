62bda3b257265dec6dc87a37a7f4e3db1f642341
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-19 15:01:36 +0200

    Settings: Allow disabling color inversion support [2/2]
    Via an overlay
    Some kernels won't support that no more

--- src/com/android/settings/accessibility/ColorInversionPreferenceController.java
+++ src/com/android/settings/accessibility/ColorInversionPreferenceController.java
@@ -42,6 +42,8 @@ public class ColorInversionPreferenceController extends BasePreferenceController
 
     @Override
     public int getAvailabilityStatus() {
-        return AVAILABLE;
+        final boolean available = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_displayInversionAvailable);
+        return available ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 }
