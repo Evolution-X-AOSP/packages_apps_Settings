4eb03a7f6c0df7d5ad69134ca8f99b8a080f0c53
Ivan Martynov <johnivan19999@gmail.com>
2023-01-26 21:07:01 +0200

    Settings: Remove check of Build Status for using OTA

--- src/com/android/settings/system/SystemUpdatePreferenceController.java
+++ src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -47,10 +47,6 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        String buildtype = SystemProperties.get(OTA_BUILD_TYPE_PROP,"unofficial");
-        if (!mUm.isAdminUser() || !buildtype.equalsIgnoreCase("official")){
-            return UNSUPPORTED_ON_DEVICE;
-        }
         try {
             PackageManager pm = mContext.getPackageManager();
             pm.getPackageInfo(OTA_APP_PACKAGE, PackageManager.GET_ACTIVITIES);
