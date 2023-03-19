f5c6886abbace24fcd8d318462f72e899b3ea5c3
Joey Huab <joey@evolution-x.org>
2023-03-19 06:27:51 +0000

    [SQUASH] Settings: Reverts after QPR2 changes
    Revert "Settings: Follow navbar color in base"
    
    This reverts commit 724af4be65676d601b6fa2d901711c752272026c.
    
    Revert "Settings: allow disable of screenshot shutter sound [2/2]"
    
    This reverts commit e4ff3f83be2fc6b0dcdffb8792c1fe9f133a9c09.
    
    Revert "Settings: Add preference for toggling the wallpaper zooming animation"
    
    This reverts commit 49a19916a1d17e96cb7fec802b016665c9c341c6.
    
    Revert "Settings: Allow user configurable fingerprint wake-and-unlock"
    
    This reverts commit ed14218afcfa4ddb1a7df305970d37c76d255fba.
    
    Revert "Settings: Allow setting wake for notification separately from doze [2/2]"
    
    This reverts commit 2c44c28a03d349f5cf37c0a88fc70c0ccb83b7fa.

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -310,8 +310,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new EmergencyTonePreferenceController(context, fragment, lifecycle);
         final VibrateIconPreferenceController vibrateIconPreferenceController =
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
-        final ScreenshotSoundPreferenceController screenshotSoundPreferenceController =
-                new ScreenshotSoundPreferenceController(context, fragment, lifecycle);
 
         controllers.add(dialPadTonePreferenceController);
         controllers.add(inCallNotificationsPreferenceController);
@@ -325,7 +323,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
-        controllers.add(screenshotSoundPreferenceController);
         controllers.add(new PreferenceCategoryController(context,
                 "other_sounds_and_vibrations_category").setChildren(
                 Arrays.asList(dialPadTonePreferenceController,
@@ -338,8 +335,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                         vibrateIconPreferenceController,
                         dockAudioMediaPreferenceController,
                         bootSoundPreferenceController,
-                        emergencyTonePreferenceController,
-                        screenshotSoundPreferenceController)));
+                        emergencyTonePreferenceController)));
 
         return controllers;
     }
