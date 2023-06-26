47ab7daab25d9c5be309496b589a2fb803e988bb
Joey Huab <joey@evolution-x.org>
2023-06-26 17:32:32 +0900

    Settings: Set default animation scales back to 1

--- src/com/android/settings/development/TransitionAnimationScalePreferenceController.java
+++ src/com/android/settings/development/TransitionAnimationScalePreferenceController.java
@@ -38,7 +38,7 @@ public class TransitionAnimationScalePreferenceController extends
     @VisibleForTesting
     static final int TRANSITION_ANIMATION_SCALE_SELECTOR = 1;
     @VisibleForTesting
-    static final float DEFAULT_VALUE = 0.8f;
+    static final float DEFAULT_VALUE = 1f;
 
     private final IWindowManager mWindowManager;
     private final String[] mListValues;
