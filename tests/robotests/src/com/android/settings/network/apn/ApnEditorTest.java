08120185580d47c02a88c11a1ec69c6372e91de7
Weng Su <wengsu@google.com>
2023-07-07 19:52:04 +0800

    [RESTRICT AUTOMERGE] Restrict ApnEditor settings
    - Finish ApnEditor settings if user is not an admin
    
    - Finish ApnEditor settings if user has DISALLOW_CONFIG_MOBILE_NETWORKS restriction
    
    Bug: 279902472
    Test: manual test
    atest -c ApnEditorTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ef2fd5b7cd74426568e8e82fb0dcddbfdaa943bf)
    Merged-In: Iecdbbff7e21dfb11e3ba385858747a220cfd3e04
    Change-Id: Iecdbbff7e21dfb11e3ba385858747a220cfd3e04

--- tests/robotests/src/com/android/settings/network/apn/ApnEditorTest.java
+++ tests/robotests/src/com/android/settings/network/apn/ApnEditorTest.java
@@ -37,6 +37,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.PersistableBundle;
 import android.telephony.CarrierConfigManager;
+import android.os.UserManager;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -102,6 +103,8 @@ public class ApnEditorTest {
     @Mock
     private FragmentActivity mActivity;
     @Mock
+    private UserManager mUserManager;
+    @Mock
     private ProxySubscriptionManager mProxySubscriptionMgr;
     @Mock
     private CarrierConfigManager mCarrierConfigManager;
@@ -129,9 +132,10 @@ public class ApnEditorTest {
         doReturn(mContext.getTheme()).when(mActivity).getTheme();
         doReturn(mContext.getContentResolver()).when(mActivity).getContentResolver();
 
-        doReturn(mCarrierConfigManager).when(mContext)
-                .getSystemService(Context.CARRIER_CONFIG_SERVICE);
-        doReturn(mBundle).when(mCarrierConfigManager).getConfigForSubId(anyInt());
+        doReturn(mUserManager).when(mContext).getSystemService(UserManager.class);
+        doReturn(true).when(mUserManager).isAdminUser();
+        doReturn(false).when(mUserManager)
+                .hasUserRestriction(UserManager.DISALLOW_CONFIG_MOBILE_NETWORKS);
 
         setMockPreference(mContext);
         mApnEditorUT.mApnData = new FakeApnData(APN_DATA);
@@ -469,6 +473,27 @@ public class ApnEditorTest {
     @Test
     public void formatInteger_shouldIgnoreNonIntegers() {
         assertThat(ApnEditor.formatInteger("not an int")).isEqualTo("not an int");
+    }
+
+    @Test
+    @Config(shadows = ShadowFragment.class)
+    public void onCreate_notAdminUser_shouldFinish() {
+        doReturn(false).when(mUserManager).isAdminUser();
+
+        mApnEditorUT.onCreate(null);
+
+        verify(mApnEditorUT).finish();
+    }
+
+    @Test
+    @Config(shadows = ShadowFragment.class)
+    public void onCreate_hasUserRestriction_shouldFinish() {
+        doReturn(true).when(mUserManager)
+                .hasUserRestriction(UserManager.DISALLOW_CONFIG_MOBILE_NETWORKS);
+
+        mApnEditorUT.onCreate(null);
+
+        verify(mApnEditorUT).finish();
     }
 
     @Test
