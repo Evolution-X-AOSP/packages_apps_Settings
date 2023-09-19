38fa50a48320f049896a4db49dd08a2b23f93293
elpaablo <donopwhile1@gmail.com>
2023-09-19 07:18:20 +0200

    Settings: Implement a dynamic summary that shows fastcharge status

--- src/com/android/settings/fuelgauge/FastChargingPreferenceController.java
+++ src/com/android/settings/fuelgauge/FastChargingPreferenceController.java
@@ -21,37 +21,90 @@ import android.os.RemoteException;
 import android.util.Log;
 
 import androidx.preference.Preference;
-
-import com.android.settings.core.BasePreferenceController;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
 
-import vendor.lineage.fastcharge.V1_0.IFastCharge;
+import com.android.settingslib.core.lifecycle.events.OnStart;
 
 import java.util.NoSuchElementException;
 
-public class FastChargingPreferenceController extends BasePreferenceController {
+import vendor.lineage.fastcharge.V1_0.IFastCharge;
+import vendor.lineage.fastcharge.V1_0.IRestrictedCurrent;
+
+public class FastChargingPreferenceController extends BasePreferenceController
+        implements OnStart {
 
     private static final String KEY_FAST_CHARGING = "fast_charging";
     private static final String TAG = "FastChargingPreferenceController";
 
     private final boolean DEBUG = false;
-    private Context mContext = null;
+
+    private IFastCharge mFastChargeService = null;
+    private IRestrictedCurrent mRestrictedCurrentService = null;
+
+    private Preference mFastChargePref;
+    private boolean mEnabled = true;
 
     public FastChargingPreferenceController(Context context) {
         super(context, KEY_FAST_CHARGING);
-        mContext = context;
+        String iStr = "IFastCharge";
         if (!mContext.getResources().getBoolean(R.bool.config_lineageFastChargeSupported)) return;
+        try {
+            mFastChargeService = IFastCharge.getService();
+            iStr = "IRestrictedCurrent";
+            mRestrictedCurrentService = IRestrictedCurrent.getService();
+        } catch (NoSuchElementException | RemoteException e) {
+            if (DEBUG) Log.e(TAG, "Failed to get " + iStr + " interface", e);
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mFastChargePref = screen.findPreference(KEY_FAST_CHARGING);
+    }
+
+    @Override
+    public void onStart() {
+        updateSummary();
     }
 
     @Override
     public int getAvailabilityStatus() {
+        if (!mContext.getResources().getBoolean(R.bool.config_lineageFastChargeSupported)) return UNSUPPORTED_ON_DEVICE;
+        return mFastChargeService != null ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        boolean enabled;
+        int current;
         try {
-            if (!mContext.getResources().getBoolean(R.bool.config_lineageFastChargeSupported)) return UNSUPPORTED_ON_DEVICE;
-            return IFastCharge.getService() != null ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
-        } catch (NoSuchElementException | RemoteException e) {
-            if (DEBUG) Log.e(TAG, "Failed to get IFastCharge interface", e);
+            enabled = mFastChargeService.isEnabled();
+        } catch (RemoteException e) {
+            if (DEBUG) Log.e(TAG, "isEnabled failed", e);
+            return "";
         }
-        return UNSUPPORTED_ON_DEVICE;
+        if (enabled) {
+            return mContext.getString(R.string.string_enabled);
+        }
+
+        try {
+            current = mRestrictedCurrentService.getRestrictedCurrent();
+        } catch (RemoteException e) {
+            if (DEBUG) Log.e(TAG, "getRestrictedCurrent failed", e);
+            return mContext.getString(R.string.string_disabled);
+        }
+
+        return mContext.getString(R.string.fastcharge_disabled_extended_summary,
+                String.valueOf(current) + " mA");
+    }
+
+    private void updateSummary() {
+        if (mFastChargePref != null) {
+            mFastChargePref.setSummary(getSummary());
+        }
     }
 }
