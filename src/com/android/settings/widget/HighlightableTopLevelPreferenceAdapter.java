5bda3acdb110de382084ca8bdfaa40a775b45b2a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-25 17:21:07 +0800

    settings: Prevent crash when updating preference background
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/settings/widget/HighlightableTopLevelPreferenceAdapter.java
+++ src/com/android/settings/widget/HighlightableTopLevelPreferenceAdapter.java
@@ -229,23 +229,43 @@ public class HighlightableTopLevelPreferenceAdapter extends PreferenceGroupAdapt
 
     private void addHighlightBackground(PreferenceViewHolder holder) {
         final View v = holder.itemView;
+        if (v == null) return;
         v.setBackgroundResource(RES_HIGHLIGHTED_BACKGROUND);
-        ((TextView) v.findViewById(android.R.id.title)).setTextColor(mTitleColorHighlight);
-        ((TextView) v.findViewById(android.R.id.summary)).setTextColor(mSummaryColorHighlight);
-        final Drawable drawable = ((ImageView) v.findViewById(android.R.id.icon)).getDrawable();
-        if (drawable != null) {
-            drawable.setTint(mIconColorHighlight);
+        TextView titleTextView = v.findViewById(android.R.id.title);
+        if (titleTextView != null) {
+            titleTextView.setTextColor(mTitleColorHighlight);
+        }
+        TextView summaryTextView = v.findViewById(android.R.id.summary);
+        if (summaryTextView != null) {
+            summaryTextView.setTextColor(mSummaryColorHighlight);
+        }
+        ImageView iconImageView = v.findViewById(android.R.id.icon);
+        if (iconImageView != null) {
+            Drawable drawable = iconImageView.getDrawable();
+            if (drawable != null) {
+                drawable.setTint(mIconColorHighlight);
+            }
         }
     }
 
     private void removeHighlightBackground(PreferenceViewHolder holder) {
         final View v = holder.itemView;
+        if (v == null) return;
         v.setBackgroundResource(RES_NORMAL_BACKGROUND);
-        ((TextView) v.findViewById(android.R.id.title)).setTextColor(mTitleColorNormal);
-        ((TextView) v.findViewById(android.R.id.summary)).setTextColor(mSummaryColorNormal);
-        final Drawable drawable = ((ImageView) v.findViewById(android.R.id.icon)).getDrawable();
-        if (drawable != null) {
-            drawable.setTint(mIconColorNormal);
+        TextView titleTextView = v.findViewById(android.R.id.title);
+        if (titleTextView != null) {
+            titleTextView.setTextColor(mTitleColorNormal);
+        }
+        TextView summaryTextView = v.findViewById(android.R.id.summary);
+        if (summaryTextView != null) {
+            summaryTextView.setTextColor(mSummaryColorNormal);
+        }
+        ImageView iconImageView = v.findViewById(android.R.id.icon);
+        if (iconImageView != null) {
+            Drawable drawable = iconImageView.getDrawable();
+            if (drawable != null) {
+                drawable.setTint(mIconColorNormal);
+            }
         }
     }
 
