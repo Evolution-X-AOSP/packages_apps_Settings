5ee2ecadddd5b360fbd459c7e0d61f4eb1b0be5f
Joey Huab <joey@evolution-x.org>
2024-01-02 19:06:08 +0900

    Settings: Make battery cycle count optional

--- src/com/android/settings/deviceinfo/batteryinfo/BatteryCycleCountPreferenceController.java
+++ src/com/android/settings/deviceinfo/batteryinfo/BatteryCycleCountPreferenceController.java
@@ -36,7 +36,8 @@ public class BatteryCycleCountPreferenceController extends BasePreferenceControl
 
     @Override
     public int getAvailabilityStatus() {
-        return AVAILABLE;
+        return mContext.getResources().getBoolean(R.bool.config_showBatteryChargeCycles)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
